6e6acfb6cb4e0093281aac7f871b0e09b170909c
Andrew Llewellyn <allewellyn@gmail.com>
2017-03-21 11:54:39 -0500

    Initial Commit

---
+++ Clinitech/Clinitech/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinitech
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SerialCommunicator sc = new SerialCommunicator();
+            sc.EstablishConnection();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Clinitech/Clinitech/SerialCommunicator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace Clinitech
+{
+    class SerialCommunicator
+    {
+        SerialPort sp;
+        public SerialCommunicator()
+        {
+
+        }
+        public void EstablishConnection()
+        {
+            sp = new SerialPort();
+            sp.BaudRate = 9600;
+            string[] portNames = SerialPort.GetPortNames(); 
+        }
+    }
+}
